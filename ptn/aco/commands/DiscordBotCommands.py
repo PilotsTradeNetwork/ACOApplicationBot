c0e1d02a297723f4a627913ba7b886b7a690f21e
Graeme Cruickshank <gcruickshank1985@gmail.com>
2021-11-14 19:46:37 +0100

    eng/PR-5 Track the granting of member role in a DB
    Updated to track the time of the member role, and check whether the user has had the role for 14 days

--- ptn/aco/commands/DiscordBotCommands.py
+++ ptn/aco/commands/DiscordBotCommands.py
@@ -1,11 +1,14 @@
+import datetime
 import os
 import sys
 
+import discord
 from discord.ext import commands
 from discord_slash.utils.manage_commands import remove_all_commands
 
-from ptn.aco.constants import bot_guild_id, TOKEN, get_bot_control_channel
+from ptn.aco.constants import bot_guild_id, TOKEN, get_bot_control_channel, get_member_role_id, bot
 from ptn.aco._metadata import __version__
+from ptn.aco.database.database import affiliator_db, affiliator_lock, affiliator_conn
 
 
 class DiscordBotCommands(commands.Cog):
@@ -98,3 +101,68 @@ class DiscordBotCommands(commands.Cog):
         """
         print(f'User {ctx.author} requested the version: {__version__}.')
         await ctx.send(f"Avast Ye Landlubber! {self.bot.user.name} is on version: {__version__}.")
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        """
+        We create a listener for the member update event.
+
+        :returns: None
+        """
+
+        # Check if the user had the member role initially.
+        bot_guild = bot.get_guild(bot_guild_id())
+        member_role = discord.utils.get(bot_guild.roles, id=get_member_role_id())
+        initial = member_role in before.roles
+        post = member_role in after.roles
+
+        # Compare them, if they changed we need to do something
+        if initial != post:
+            change = 'Added' if post else 'Removed'
+            print(f'Member role {change} for user: {before}')
+
+            if post:
+                # Add the role - add to DB
+                affiliator_db.execute(
+                    "SELECT * FROM membertracking WHERE discord_username LIKE (?)", (f'%{before}%',)
+                )
+                user = affiliator_db.fetchone()
+
+                if user:
+                    print(f'Some problem - user {before} is already in the tracking DB with the data: {dict(user)}.')
+                    return
+
+                try:
+                    affiliator_lock.acquire()
+                    affiliator_db.execute(
+                        '''INSERT INTO membertracking VALUES(NULL, ?, ?)''',
+                        (
+                            str(before),
+                            datetime.datetime.now()
+                        )
+                    )
+                    affiliator_conn.commit()
+                finally:
+                    affiliator_lock.release()
+                    print(f'Member tracking status was added into DB for user: {before} at {datetime.datetime.now()}')
+            else:
+                # Remove the role - delete from DB if it is there
+                affiliator_db.execute(
+                    "SELECT * FROM membertracking WHERE discord_username LIKE (?)", (f'%{before}%',)
+                )
+                user = dict(affiliator_db.fetchone())
+                if not user:
+                    print(f'User had the member role removed, but was not in the DB: {before}')
+                    return
+
+                try:
+                    affiliator_lock.acquire()
+                    affiliator_db.execute(
+                        "DELETE FROM membertracking WHERE discord_username LIKE (?)", (f'%{before.name}%',)
+                    )
+                    affiliator_conn.commit()
+                    print(f'Member tracking status was deleted from DB for user: {before} at {datetime.datetime.now()}')
+                finally:
+                    affiliator_lock.release()
+
+
